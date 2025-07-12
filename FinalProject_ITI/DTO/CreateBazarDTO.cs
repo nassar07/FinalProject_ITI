5e0d726deaceed3a526ddec7bf2159d81c9b182d
Nahed badr <nahedbadr33344@gmail.com>
2025-07-12 17:31:36 +0300

    Edit bazar.cs  ,bazarDTO, bazarcontroller

--- FinalProject_ITI/DTO/CreateBazarDTO.cs
+++ FinalProject_ITI/DTO/CreateBazarDTO.cs
@@ -1,20 +1,14 @@
-﻿using FinalProject_ITI.Models;
-
-namespace FinalProject_ITI.Models
+﻿namespace FinalProject_ITI.DTO
 {
-    public class Bazar
+    public class CreateBazarDTO
     {
-        public int Id { get; set; }
+
         public string Title { get; set; }
         public DateTime EventDate { get; set; }
         public string StartTime { get; set; }
         public string EndTime { get; set; }
         public string Location { get; set; }
         public string Entry { get; set; }
-        public ICollection<BazarBrand> BazarBrands { get; set; }
     }
 }
-
-
-
 
