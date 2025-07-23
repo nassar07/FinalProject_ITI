f47a7f2bc479205403a737fdf67f1e1b5b68ea3c
mohamedShawki <[muhammad.shawkii@gmail.com]>
2025-07-23 18:35:53 +0300

    Fix Error

--- FinalProject_ITI/DTO/BrandDto.cs
+++ FinalProject_ITI/DTO/BrandDto.cs
@@ -3,10 +3,8 @@
 namespace FinalProject_ITI.DTO;
 public class BrandDTO
 {
-    public class BrandDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
     public string? Description { get; set; }
     public string? Address { get; set; }
     public string? Image { get; set; }
