67c12466fb5b34e85c8994c87e4c973ca485c99e
mohamedShawki <[muhammad.shawkii@gmail.com]>
2025-07-26 15:14:54 +0300

    now admin account should be created on first run and seed role to it's table successfully

--- FinalProject_ITI/Program.cs
+++ FinalProject_ITI/Program.cs
@@ -1,3 +1,4 @@
+using FinalProject_ITI.Helpers;
 using FinalProject_ITI.Models;
 using FinalProject_ITI.Repositories.Implementations;
 using FinalProject_ITI.Repositories.Interfaces;
@@ -47,40 +48,7 @@ public class Program
 
         var app = builder.Build();
 
-        using (var scope = app.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-
-            string[] roles = { "USER", "ADMIN", "DeliveryBoy" };
-            foreach (var roleName in roles)
-            {
-                if (!await roleManager.RoleExistsAsync(roleName))
-                {
-                    await roleManager.CreateAsync(new IdentityRole(roleName));
-                }
-            }
-
-            string adminEmail = "admin@admin.com";
-            string adminPassword = "123";
-            var adminUser = await userManager.FindByEmailAsync(adminEmail);
-
-            if (adminUser == null)
-            {
-                var newAdmin = new ApplicationUser
-                {
-                    UserName = adminEmail,
-                    Email = adminEmail
-                };
-
-                var result = await userManager.CreateAsync(newAdmin, adminPassword);
-                if (result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(newAdmin, "ADMIN");
-                }
-            }
-        }
+        await SeedRoles.SeedRolesAndAdminAsync(app);
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
@@ -96,8 +64,9 @@ public class Program
                     .AllowAnyMethod()
                     .AllowAnyHeader();
         });
-        app.UseAuthorization();
 
+        app.UseAuthentication();
+        app.UseAuthorization();
 
         app.MapControllers();
 
